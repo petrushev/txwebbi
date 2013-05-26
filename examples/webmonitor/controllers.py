f3551c35fffd6bd4c8ae9318a6fca5c479979608
Baze Petrushev <b.petrushev@gmail.com>
2013-05-26 03:29:05 +0200

    Added `webmonitor` example

---
+++ examples/webmonitor/controllers.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from twisted.web.http import NOT_FOUND, TEMPORARY_REDIRECT
+from twisted.internet import reactor
+
+from txwebby import BaseController
+
+class NotFound(BaseController):
+    def init(self):
+        self.request.setResponseCode(NOT_FOUND)
+        self.request.write('page not found. [404]')
+        self.finish()
+
+class Index(BaseController):
+    def init(self):
+        self.request.write('Hello! This request will be logged.')
+        self.finish()
+
+class Log(BaseController):
+    def init(self):
+        # the factory object holds persistant info through different web requests
+        factory = self.request.channel.factory
+        if not hasattr(factory, 'sentinel'):
+            factory.sentinel = set() # sentinel will be a set of Log controllers
+
+        # add this log controller to the factory's sentinel
+        factory.sentinel.add(self)
+
+    def prompt(self, frontHandler):
+        # on each request, the prompt method of each controller in the factory's sentinel should be called
+        # the easiest way to achieve this is to add a deferred on the BaseController __init__
+        # or to attach a hook after the front handler process starts
+        self.request.write('%s %s <br/>\n' % \
+                           (frontHandler.method, frontHandler.path))
+
+    def notifyDisconnect(self, reason):
+        # remove this log controller from the factory's sentinel
+        factory = self.request.channel.factory
+        if self in factory.sentinel:
+            factory.sentinel.remove(self)
---
+++ examples/webmonitor/main.py
@@ -0,0 +1,62 @@
+from os.path import join as pathjoin
+from os.path import dirname
+
+from werkzeug.routing import Rule, Map
+
+from twisted.internet import reactor
+
+from txwebby import bootstrapWebServer, bootstrapCommonFrontHandler
+
+from twisted.internet.task import deferLater
+from twisted.python import log
+
+import controllers
+
+template_path = pathjoin(dirname(__file__), 'templates/')
+
+url_map = Map([
+    Rule('/', endpoint = controllers.Index),
+    Rule('/log', endpoint = controllers.Log)
+])
+
+BaseFrontHandler = bootstrapCommonFrontHandler(url_map, template_path, controllers.NotFound)
+
+class FrontHandler(BaseFrontHandler):
+    """Extended FrontHandler to accomodate prompting of requests to factory's sentinel"""
+
+    def process(self):
+        BaseFrontHandler.process(self)
+
+        # defer task of prompting
+        init_task = deferLater(reactor, 0, self.prompt)
+        init_task.addErrback(self.process_err)
+
+    def process_err(self, reason):
+        log.err('prompt start err: '+ reason.getErrorMessage())
+
+    def prompt(self):
+        """Send prompt to each log in the factory's sentinel"""
+        factory = self.channel.factory
+        if not hasattr(factory, 'sentinel'):
+            factory.sentinel = set()
+
+        for logCtrl in factory.sentinel:
+            if self.controller == logCtrl: continue
+
+            init_task = deferLater(reactor, 0, logCtrl.prompt, self)
+            init_task.addErrback(self.prompt_err)
+
+    def prompt_err(self, reason):
+        log.err('log prompt err: '+ reason.getErrorMessage())
+
+WebServer = bootstrapWebServer(FrontHandler)
+
+# the following 3 lines are needed in case if we use the `twistd` infrastructure:
+# twistd -ny main.py
+from twisted.application.service import Application
+application = Application('The Web')
+WebServer(port = 8070).setServiceParent(application)
+
+if __name__=='__main__':
+    reactor.callLater(0, WebServer(port = 8070).startService)
+    reactor.run()
